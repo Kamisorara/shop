8113cdf9ea0612aa102a6910c3a331952a86ee3b
kamisora <1210281722@qq.com>
2022-05-11 17:15:07 +0800

    主页基本设计完成

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,186 +1,154 @@
 <template>
-  <div style="border-bottom: 1px solid #dfdfdf; font-size: 14px">
-    <div
-      style="
+	<div style="border-bottom: 1px solid #dfdfdf; font-size: 14px">
+		<div style="
         display: flex;
         height: 40px;
         padding: 0 200px;
         background-color: #ea9573;
-      "
-    >
-      <div class="top-title">会员中心</div>
-      <div class="top-title" style="margin-left: 20px">提交bug</div>
-      <div class="top-title" style="margin-left: 20px">充值</div>
-      <div
-        class="top-title"
-        style="line-height: 40px; color: #ffffff; margin-left: 20px"
-      >
-        一键三连
-      </div>
-      <div style="flex: 1; display: flex">
-        <div style="flex: 1"></div>
-        <div style="width: 170px; display: flex">
-          <el-button
-            v-if="!isLogin"
-            type="text"
-            style="color: #ffffff"
-            class="top-title"
-            @click="toLogin"
-            >登录
-          </el-button>
-          <el-button
-            v-if="!isLogin"
-            type="text"
-            style="color: #ffffff; margin-right: 10px"
-            class="top-title"
-            @click="toRegister"
-            >注册</el-button
-          >
-          <div style="margin-right: 10px" v-if="isLogin">
-            <el-dropdown>
-              <span class="el-dropdown-link" style="font-size: 15px">
-                xxx,你好<i class="el-icon-arrow-down el-icon--right"></i>
-              </span>
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>
-                  <router-link :to="{ name: 'homePage' }">
-                    个人中心
-                  </router-link>
-                </el-dropdown-item>
-                <el-dropdown-item @click.native="logout">退出</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </div>
+      ">
+			<div class="top-title">会员中心</div>
+			<div class="top-title" style="margin-left: 20px">提交bug</div>
+			<div class="top-title" style="margin-left: 20px">充值</div>
+			<div class="top-title" style="line-height: 40px; color: #ffffff; margin-left: 20px">
+				一键三连
+			</div>
+			<div style="flex: 1; display: flex">
+				<div style="flex: 1"></div>
+				<div style="width: 170px; display: flex">
+					<el-button v-if="!isLogin" type="text" style="color: #ffffff" class="top-title" @click="toLogin">登录
+					</el-button>
+					<el-button v-if="!isLogin" type="text" style="color: #ffffff; margin-right: 10px" class="top-title"
+						@click="toRegister">注册</el-button>
+					<div style="margin-right: 10px" v-if="isLogin">
+						<el-dropdown>
+							<span class="el-dropdown-link" style="font-size: 15px">
+								xxx,你好<i class="el-icon-arrow-down el-icon--right"></i>
+							</span>
+							<el-dropdown-menu slot="dropdown">
+								<el-dropdown-item>
+									<router-link :to="{ name: 'homePage' }">
+										个人中心
+									</router-link>
+								</el-dropdown-item>
+								<el-dropdown-item @click.native="logout">退出</el-dropdown-item>
+							</el-dropdown-menu>
+						</el-dropdown>
+					</div>
 
-          <div
-            style="
+					<div style="
               background-color: rgb(223 221 221 / 40%);
               display: flex;
               width: 100px;
               padding: 0 10px;
-            "
-            class="top-title"
-          >
-            <img
-              src="../assets/img2.png"
-              style="
+            " class="top-title">
+						<img src="../assets/img2.png" style="
                 width: 20px;
                 height: 20px;
                 margin-top: 10px;
                 margin-right: 5px;
-              "
-            />
-            <el-button type="text" style="color: #ffffff" class="top-title"
-              >购物车</el-button
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-    <div style="margin: 0 200px; height: 80px; line-height: 80px">
-      <div style="display: flex">
-        <img
-          src="../assets/logo.png"
-          style="cursor: pointer; height: 60px; margin-top: 10px"
-        />
-        <div class="title" style="margin-left: 30px" @click="toIndex">首页</div>
-        <div class="title">电子产品</div>
-        <div class="title">家具家电</div>
-        <div class="title">母婴用品</div>
-        <div class="title">零食</div>
-        <div class="title">联系我们</div>
-        <div style="flex: 1; text-align: right">
-          <el-input
-            placeholder="请输入查找的商品"
-            v-model="input"
-            style="width: 500px"
-          >
-            <el-button slot="append" icon="el-icon-search"></el-button>
-          </el-input>
-        </div>
-      </div>
-    </div>
-  </div>
+              " />
+						<el-button type="text" style="color: #ffffff" class="top-title">购物车</el-button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div style="margin: 0 200px; height: 80px; line-height: 80px">
+			<div style="display: flex">
+				<img src="../assets/logo.png" style="cursor: pointer; height: 60px; margin-top: 10px" />
+				<div class="title" style="margin-left: 30px" @click="toIndex">首页</div>
+				<div class="title">电子产品</div>
+				<div class="title">家具家电</div>
+				<div class="title">母婴用品</div>
+				<div class="title">零食</div>
+				<div class="title">联系我们</div>
+				<div style="flex: 1; text-align: right">
+					<el-input placeholder="请输入查找的商品" v-model="input" style="width: 500px">
+						<el-button slot="append" icon="el-icon-search"></el-button>
+					</el-input>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>ge
 
 <script>
-export default {
-  data() {
-    return {
-      input: "",
-      isLogin: false,
-    };
-  },
-  mounted() {},
-  methods: {
-    homePage() {
-      this.$router.push({
-        path: "/home/index",
-      });
-    },
-    toIndex() {
-      this.$router.push("/commodity");
-    },
-    toLogin() {
-      this.$router.push("/login");
-    },
-    toRegister() {
-      this.$router.push("/register");
-    },
-    logout() {
-      this.$axios
-        .get("/user/logout")
-        .then((res) => {
-          this.clearMethod();
-          this.$store.commit("resetState"); //调用store里面的方法来清空token
-          this.$message({
-            message: res.data.msg,
-            type: "success",
-          });
-          setTimeout(() => {
-            //需要延迟的代码 :1秒后延迟跳转到首页，可以加提示什么的
-            this.$router.push("/");
-            //延迟时间：1秒
-          }, 1000);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-    //清除localStorage中的token 和userName
-    clearMethod() {
-      localStorage.clear();
-      sessionStorage.clear();
-    },
-  },
-};
+	export default {
+		data() {
+			return {
+				input: "",
+				isLogin: false,
+			};
+		},
+		mounted() {},
+		methods: {
+			homePage() {
+				this.$router.push({
+					path: "/home/index",
+				});
+			},
+			toIndex() {
+				this.$router.push("/commodity");
+			},
+			toLogin() {
+				this.$router.push("/login");
+			},
+			toRegister() {
+				this.$router.push("/register");
+			},
+			logout() {
+				this.$axios
+					.get("/user/logout")
+					.then((res) => {
+						this.clearMethod();
+						this.$store.commit("resetState"); //调用store里面的方法来清空token
+						this.$message({
+							message: res.data.msg,
+							type: "success",
+						});
+						setTimeout(() => {
+							//需要延迟的代码 :1秒后延迟跳转到首页，可以加提示什么的
+							this.$router.push("/");
+							//延迟时间：1秒
+						}, 1000);
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			},
+			//清除localStorage中的token 和userName
+			clearMethod() {
+				localStorage.clear();
+				sessionStorage.clear();
+			},
+		},
+	};
 </script>
 
 <style scoped>
-.top-title:hover {
-  cursor: pointer;
-  color: #e5e5e5 !important;
-}
+	.top-title:hover {
+		cursor: pointer;
+		color: #e5e5e5 !important;
+	}
 
-.title {
-  font-weight: bold;
-  font-size: 18px;
-}
+	.title {
+		font-weight: bold;
+		font-size: 18px;
+	}
 
-.title:hover {
-  color: #c250c6 !important;
-}
+	.title:hover {
+		color: #c250c6 !important;
+	}
 
-.top-title {
-  padding-right: 20px;
-  border-right: 1px solid rgb(230 230 230 / 40%);
-  line-height: 40px;
-  color: #ffffff;
-}
+	.top-title {
+		padding-right: 20px;
+		border-right: 1px solid rgb(230 230 230 / 40%);
+		line-height: 40px;
+		color: #ffffff;
+	}
 
-.title {
-  margin-right: 20px;
-  cursor: pointer;
-  color: gray;
-}
+	.title {
+		margin-right: 20px;
+		cursor: pointer;
+		color: gray;
+	}
 </style>
--- src/views/commodity.vue
+++ src/views/commodity.vue
@@ -1,309 +1,304 @@
 <template>
-  <div class="main">
-    <el-container>
-      <el-aside width="200px">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span style="font-size: 20px">商品分类</span>
-          </div>
-          <div
-            v-for="(item, index) in calssifications"
-            :key="index"
-            class="text item"
-          >
-            <i :class="item.icon"></i>
-            <a href="#" style="text-decoration: none; color: darkgrey">{{
+	<div class="main">
+		<el-container>
+			<el-aside width="200px">
+				<el-card class="box-card">
+					<div slot="header" class="clearfix">
+						<span style="font-size: 20px">商品分类</span>
+					</div>
+					<div v-for="(item, index) in calssifications" :key="index" class="text item">
+						<i :class="item.icon"></i>
+						<a href="#" style="text-decoration: none; color: darkgrey">{{
               item.classificationName
             }}</a>
-          </div>
-        </el-card>
-      </el-aside>
-      <el-main>
-        <el-carousel indicator-position="outside" style="border-radius: 30px">
-          <el-carousel-item v-for="(item, index) in imagesbox" :key="index">
-            <img :src="item.idView" class="image" />
-          </el-carousel-item>
-        </el-carousel>
-      </el-main>
-      <el-aside width="200px">
-        <div v-show="!isLogin">
-          <div class="demo-type">
-            <div>
-              <el-avatar icon="el-icon-user-solid"></el-avatar>
-            </div>
-          </div>
-          <el-button type="text" @click="test">请登录</el-button>
-        </div>
-        <div v-show="isLogin">
-          <div class="demo-type">
-            <div>
-              <el-avatar
-                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-              >
-              </el-avatar>
-            </div>
-          </div>
-          <p style="font-size: 20px">Hi,XXX</p>
-        </div>
+					</div>
+				</el-card>
+			</el-aside>
+			<el-main>
+				<el-carousel indicator-position="outside" style="border-radius: 30px;">
+					<el-carousel-item v-for="(item, index) in imagesbox" :key="index">
+						<img :src="item.idView" class="image" />
+					</el-carousel-item>
+				</el-carousel>
+			</el-main>
+			<el-aside width="200px">
+				<div v-show="!isLogin">
+					<div class="demo-type">
+						<div>
+							<el-avatar icon="el-icon-user-solid"></el-avatar>
+						</div>
+					</div>
+					<el-button type="text" @click="toLogin">请登录</el-button>
+				</div>
+				<div v-show="isLogin">
+					<div class="demo-type">
+						<div>
+							<el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png">
+							</el-avatar>
+						</div>
+					</div>
+					<p style="font-size: 20px">Hi,XXX</p>
+				</div>
 
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span>公告板</span>
-          </div>
-          <div v-for="(item, index) in notice" :key="index" class="board_item">
-            <a href="#" style="text-decoration: none; color: darkgrey">{{
-              item.message
+				<el-card class="box-card">
+					<div slot="header" class="clearfix">
+						<span>公告板</span>
+					</div>
+					<div v-for="(item, index) in noticed" :key="index" class="board_item">
+						<a href="#" style="text-decoration: none; color: darkgrey">{{
+              item.notice
             }}</a>
-          </div>
-        </el-card>
-      </el-aside>
-    </el-container>
-    <div class="content">
-      <div
-        class="show"
-        v-for="(product, index) in products"
-        :key="index"
-        @click="toDetail(product.id)"
-      >
-        <div class="imgbg">
-          <el-image
-            :src="product.photoAddr"
-            style="height: 180px; width: 200px; margin-top: 30px"
-          ></el-image>
-        </div>
-        <div class="price" style="font-size: 20px; margin-left: 15px">
-          ¥{{ product.price }}
-        </div>
-        <div class="store">{{ product.shopName }}</div>
-        <div class="detail">
-          <div class="details">
-            月成交<mark>{{ product.sell }}笔</mark>
-          </div>
-          <div class="details">
-            评价<span>{{ product.mark }}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+					</div>
+				</el-card>
+			</el-aside>
+		</el-container>
+		<div class="content">
+			<div class="show" v-for="(product, index) in products" :key="index" @click="toDetail(product.id)">
+				<div class="imgbg">
+					<el-image :src="product.photoAddr" style="height: 180px; width: 200px; margin-top: 30px"></el-image>
+				</div>
+				<div class="price" style="font-size: 20px; margin-left: 15px">
+					¥{{ product.price }}
+				</div>
+				<div class="store">{{ product.shopName }}</div>
+				<div class="detail">
+					<div class="details">
+						月成交<mark>{{ product.sell }}笔</mark>
+					</div>
+					<div class="details">
+						评价<span>{{ product.mark }}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "commodity",
-  data() {
-    return {
-      isLogin: false,
-      notice: [
-        {
-          message: "请登录后查看",
-        },
-      ],
-      imagesbox: [
-        {
-          id: 0,
-          idView: require("../assets/advertise1.png"),
-        },
-        {
-          id: 1,
-          idView: require("../assets/advertise2.jpg"),
-        },
-        {
-          id: 2,
-          idView: require("../assets/advertise3.jpg"),
-        },
-        {
-          id: 3,
-          idView: require("../assets/advertise4.jpg"),
-        },
-        {
-          id: 4,
-          idView: require("../assets/advertise5.jpg"),
-        },
-      ],
-      products: [
-        {
-          id: 1111,
-          price: 9999,
-          shopName: "探碗揽月",
-          photoAddr: "",
-          sell: 9999,
-          mark: 9999,
-        },
-      ],
-      calssifications: [
-        {
-          id: "123123",
-          icon: "el-icon-shopping-cart-full",
-          classificationName: "xxxx",
-        },
-        {
-          id: "123123",
-          icon: "el-icon-shopping-cart-full",
-          classificationName: "xxxx",
-        },
-        {
-          id: "123123",
-          icon: "el-icon-shopping-cart-full",
-          classificationName: "xxxx",
-        },
-        {
-          id: "123123",
-          icon: "el-icon-shopping-cart-full",
-          classificationName: "xxxx",
-        },
-        {
-          id: "123123",
-          icon: "el-icon-shopping-cart-full",
-          classificationName: "xxxx",
-        },
-      ],
-    };
-  },
-  methods: {
-    toDetail(shop_id) {
-      this.$router.push({
-        path: "/details",
-        query: { id: shop_id },
-      });
-    },
-    toLogin() {
-      this.$router.push("/login");
-    },
-    getAllRecommended() {
-      this.$axios
-        .get("/shop/index/default-recommended")
-        .then((res) => {
-          console.log(res);
-          this.products = res.data.data;
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-    getClassification() {
-      this.$axios
-        .get("/shop/index/classification")
-        .then((res) => {
-          console.log(res);
-          this.calssifications = res.data.data;
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-  },
-  mounted() {
-    this.getAllRecommended();
-    this.getClassification();
-  },
-};
+	export default {
+		name: "commodity",
+		data() {
+			return {
+				isLogin: false,
+				noticed: [{
+					notice: "请登录后查看",
+				}, ],
+				imagesbox: [{
+						id: 0,
+						idView: require("../assets/advertise1.png"),
+					},
+					{
+						id: 1,
+						idView: require("../assets/advertise2.jpg"),
+					},
+					{
+						id: 2,
+						idView: require("../assets/advertise3.jpg"),
+					},
+					{
+						id: 3,
+						idView: require("../assets/advertise4.jpg"),
+					},
+					{
+						id: 4,
+						idView: require("../assets/advertise5.jpg"),
+					},
+				],
+				products: [{
+					id: 1111,
+					price: 9999,
+					shopName: "探碗揽月",
+					photoAddr: "",
+					sell: 9999,
+					mark: 9999,
+				}, ],
+				calssifications: [{
+						id: "123123",
+						icon: "el-icon-shopping-cart-full",
+						classificationName: "xxxx",
+					},
+					{
+						id: "123123",
+						icon: "el-icon-shopping-cart-full",
+						classificationName: "xxxx",
+					},
+					{
+						id: "123123",
+						icon: "el-icon-shopping-cart-full",
+						classificationName: "xxxx",
+					},
+					{
+						id: "123123",
+						icon: "el-icon-shopping-cart-full",
+						classificationName: "xxxx",
+					},
+					{
+						id: "123123",
+						icon: "el-icon-shopping-cart-full",
+						classificationName: "xxxx",
+					},
+				],
+			};
+		},
+		methods: {
+			toDetail(shop_id) {
+				this.$router.push({
+					path: "/details",
+					query: {
+						id: shop_id
+					},
+				});
+			},
+			toLogin() {
+				this.$router.push("/login");
+			},
+			getAllRecommended() {
+				this.$axios
+					.get("/shop/index/default-recommended")
+					.then((res) => {
+						console.log(res);
+						this.products = res.data.data;
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			},
+			getClassification() {
+				this.$axios
+					.get("/shop/index/classification")
+					.then((res) => {
+						console.log(res);
+						this.calssifications = res.data.data;
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			},
+
+			getAllNotice() {
+				this.$axios
+					.get("/shop/index/notice-board")
+					.then((res) => {
+						console.log(res);
+						this.noticed = res.data.data;
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			}
+		},
+		mounted() {
+			this.getAllRecommended();
+			this.getClassification();
+			this.getAllNotice();
+		},
+	};
 </script>
 <style scoped>
-.board_item {
-  text-align: left;
-  font-size: 15px;
-}
+	.board_item {
+		text-align: left;
+		font-size: 15px;
+	}
 
-/* 头像 */
-.demo-type {
-  margin-top: 60px;
-}
+	/* 头像 */
+	.demo-type {
+		margin-top: 60px;
+	}
 
-.text {
-  font-size: 14px;
-}
+	.text {
+		font-size: 14px;
+	}
 
-.item {
-  margin-bottom: 18px;
-}
+	.item {
+		margin-bottom: 18px;
+	}
 
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
+	.clearfix:before,
+	.clearfix:after {
+		display: table;
+		content: "";
+	}
 
-.clearfix:after {
-  clear: both;
-}
+	.clearfix:after {
+		clear: both;
+	}
 
-.box-card {
-  width: 190px;
-}
+	.box-card {
+		width: 190px;
+	}
 
-.el-aside {
-  color: #333;
-  text-align: center;
-}
+	.el-aside {
+		color: #333;
+		text-align: center;
+	}
 
-.el-main {
-  background-color: #f7f9fa;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
+	.el-main {
+		background-color: #f7f9fa;
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
 
-/* ----- */
-.main {
-  width: 1250px;
-  margin: 0px auto;
-}
+	/* ----- */
+	.main {
+		width: 1250px;
+		margin: 0px auto;
+	}
 
-div {
-  font-size: 12px;
-}
+	div {
+		font-size: 12px;
+	}
 
-.detail {
-  font-size: 0px;
-  background: url(#) no-repeat 190px center;
-}
+	.detail {
+		font-size: 0px;
+		background: url(#) no-repeat 190px center;
+	}
 
-.details {
-  display: inline-block;
-  padding-right: 13px;
-  border-right: 1px solid #e6e6e6;
-  text-align: center;
-  text-indent: 1.2em;
-}
+	.details {
+		display: inline-block;
+		padding-right: 13px;
+		border-right: 1px solid #e6e6e6;
+		text-align: center;
+		text-indent: 1.2em;
+	}
 
-.show {
-  width: 200px;
-  display: inline-block;
-  margin-left: 50px;
-  background-color: #e6e9ec;
-  border-radius: 30px;
-  margin-top: 25px;
-}
+	.show {
+		width: 200px;
+		display: inline-block;
+		margin-left: 50px;
+		background-color: #e6e9ec;
+		border-radius: 30px;
+		margin-top: 25px;
+	}
 
-.content {
-  font-size: 0px;
-  margin-bottom: 10px;
-}
+	.content {
+		font-size: 0px;
+		margin-bottom: 10px;
+	}
 
-.price {
-  font-size: 16px;
-  color: red;
-  font-weight: bold;
-}
+	.price {
+		font-size: 16px;
+		color: red;
+		font-weight: bold;
+	}
 
-.store {
-  color: #7f7f7f;
-  text-decoration: underline;
-  margin-bottom: 5px;
-  font-size: 15px;
-}
+	.store {
+		color: #7f7f7f;
+		text-decoration: underline;
+		margin-bottom: 5px;
+		font-size: 15px;
+	}
 
-mark {
-  background-color: transparent;
-  color: #e5c45a;
-}
+	mark {
+		background-color: transparent;
+		color: #e5c45a;
+	}
 
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 18px;
-  opacity: 0.75;
-  line-height: 300px;
-  margin: 0;
-}
+	.el-carousel__item h3 {
+		color: #475669;
+		font-size: 18px;
+		opacity: 0.75;
+		line-height: 300px;
+		margin: 0;
+	}
 </style>
